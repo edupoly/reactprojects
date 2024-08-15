3a8ae633b564c71d1b831cf8453783e4d909ed19
edupoly <info.edupoly@gmail.com>
2024-08-15 20:47:33 +0530

    sdfdsfsdf

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container bg-light vh-100'>
+      <h1>
+      <i class="bi bi-pc-display"></i>
+        Edupoly
+      </h1>
+    </div>
+  );
+}
+
+export default App;
