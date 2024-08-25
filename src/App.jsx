f00057a99f16de46a156b70e71b2636318fbe929
edupoly <info.edupoly@gmail.com>
2024-08-25 11:03:41 +0530

    zxc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import pizzas from './pizzas.json'
+import { useEffect } from 'react';
 function App() {
+  useEffect(()=>{
+    console.log(pizzas)
+  })
   return (
     <div className='container bg-light vh-100'>
       <h1>
